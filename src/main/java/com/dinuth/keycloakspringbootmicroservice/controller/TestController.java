e7a9e3c790315feccee915f6d749bd46d13c7f91
Sanjay Sachdev <sasachde@redhat.com>
2021-10-18 18:08:04 +0530

    Modified the context path

--- src/main/java/com/dinuth/keycloakspringbootmicroservice/controller/TestController.java
+++ src/main/java/com/dinuth/keycloakspringbootmicroservice/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.security.RolesAllowed;
 
 @RestController
-@RequestMapping("/test")
+//@RequestMapping("/test")
 public class TestController {
 
     @RequestMapping(value = "/anonymous", method = RequestMethod.GET)
